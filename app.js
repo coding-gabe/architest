34547fe2a2895e8be9ac63b3f2bf7931d69d816a
Gabriel Gaspar <gabrielgaspar.ds@gmail.com>
2024-01-17 00:07:02 +0000

    projeto quiz

---
+++ app.js
@@ -0,0 +1,169 @@
+// Perguntas
+let pergunta_1 = "Qual é o arquiteto responsável pela construção da Torre Eiffel, em Paris?";
+let pergunta_2 = "Qual é o nome da famosa residência projetada por Frank Lloyd Wright, que se destaca por sua integração com a natureza?";
+let pergunta_3 = "Em que cidade está localizada um famoso marco arquitetônico a beira mar, onde ainda hoje funciona uma Ópera projetada por Jørn Utzon em 1959?";
+let pergunta_4 = "Qual é o estilo arquitetônico conhecido por suas altas abóbadas e arcos apontados, frequentemente associado a igrejas medievais?";
+let pergunta_5 = "Quem foi o arquiteto responsável pelo projeto do World Trade Center em Nova York?";
+let pergunta_6 = "Qual é a estrutura arquitetônica romana famosa por sua abóbada em forma de cúpula?";
+let pergunta_7 = "Quem projetou a Casa da Ópera de Guangzhou, na China?";
+let pergunta_8 = "Que famoso arquiteto é conhecido por suas casas orgânicas, muitas vezes construídas com concreto e vidro, como a Casa da Cascata?";
+let pergunta_9 = "Em que cidade italiana está localizada a famosa Torre Inclinada?";
+let pergunta_10 = "Qual é o nome do arquiteto responsável pelo projeto da Casa da Música, no Porto, Portugal?";
+let pergunta_11 = "Qual é o estilo arquitetônico caracterizado por linhas simples, funcionalidade e uso de materiais modernos, popularizado no século XX?";
+let pergunta_12 = "Quem foi o arquiteto responsável pelo projeto do Museu Guggenheim em Bilbao, conhecido por sua arquitetura arrojada e curvilínea?";
+let pergunta_13 = "Qual característica é típica da arquitetura barroca?";
+let pergunta_14 = "Qual é o nome da residência oficial do presidente dos Estados Unidos, projetada por James Hoban e construída em estilo neoclássico?";
+let pergunta_15 = "Quem foi o arquiteto que projetou o Palácio da Alvorada em Brasília, uma das principais obras da capital brasileira?";
+let pergunta_16 = "Qual é o nome do arquiteto espanhol que projetou a Basílica da Sagrada Família, em Barcelona?";
+let pergunta_17 = "Qual é o nome do estilo arquitetônico que predominou na Europa durante os séculos XVII e XVIII, caracterizado por detalhes ornamentados e simétricos?";
+let pergunta_18 = "Quem foi o arquiteto responsável pela construção da Grande Pirâmide de Gizé, no Egito?";
+let pergunta_19 = "Em que cidade está localizado o famoso Coliseu, um dos maiores anfiteatros da Roma Antiga?";
+let pergunta_20 = "Qual dessas obras foi feita durante a ditadura militar do Brasil?";
+
+// Respostas
+let gabarito_1 = "Gustave Eiffel";
+let gabarito_2 = "Fallingwater (Casa da Cascata)";
+let gabarito_3 = "Sydney";
+let gabarito_4 = "Estilo Gótico";
+let gabarito_5 = "Minoru Yamasaki";
+let gabarito_6 = "Panteão";
+let gabarito_7 = "Zaha Hadid";
+let gabarito_8 = "Frank Lloyd Wright";
+let gabarito_9 = "Pisa";
+let gabarito_10 = "Rem Koolhaas";
+let gabarito_11 = "Estilo Modernista";
+let gabarito_12 = "Frank Gehry";
+let gabarito_13 = "Ornamentação extravagante e detalhada";
+let gabarito_14 = 'White House';
+let gabarito_15 = "Oscar Niemeyer";
+let gabarito_16 = "Antoni Gaudí";
+let gabarito_17 = "Barroco";
+let gabarito_18 = "Imhotep";
+let gabarito_19 = "Roma";
+let gabarito_20 = 'Sede do Partido Comunista Francês';
+//Opções
+let opcoes_1 = ['Albert Einstein', 'Gabriel Garcia Marquez', 'Thomas Edison', 'Gustave Eiffel'];
+let opcoes_2 = ["Fallingwater (Casa da Cascata)", "Robie House", "Taliesin West", "Johnson Wax Building"];
+let opcoes_3 = ['Sydney', 'Melbourne', 'Brisbane', 'Londres'];
+let opcoes_4 = ['Internacional', 'Barroco', 'Neo-clássico', 'Estilo Gótico'];
+let opcoes_5 = ['Álvaro Siza', 'Minoru Yamasaki', 'Tadao Ando', 'John Portman'];
+let opcoes_6 = ['Partenon', 'Roman Forum', 'Colosseum', 'Panteão'];
+let opcoes_7 = ['Zaha Hadid', 'I. M. Pei', 'Norman Foster', 'Richard Rogers'];
+let opcoes_8 = ['Le Corbusier', 'Mies', 'Loos', 'Frank Lloyd Wright'];
+let opcoes_9 = ['Florença', 'Napoli', 'Milão', 'Pisa'];
+let opcoes_10 = ['BIG', 'MVRDV', 'Rem Koolhaas', 'Herzog & de Meuron'];
+let opcoes_11 = ['Pós-estruturalismo', 'Hi-tech', 'Blob', 'Estilo Modernista'];
+let opcoes_12 = ['Bernard Tschumi', 'Eric Owen Moss', 'Wolf Prix', 'Frank Gehry'];
+let opcoes_13 = [ "Uso predominante de linhas retas e ângulos agudos.","Ênfase na simplicidade e funcionalidade.","Ornamentação extravagante e detalhada","Utilização de estruturas minimalistas e geométricas."];
+let opcoes_14 = ['Lincoln Memorial', 'Thomas Jefferson Memorial','White House', 'Capitol Building'];
+let opcoes_15 = ['Lúcio Costa',"Oscar Niemeyer", 'Paulo Mendes da Rocha', 'Villanova Artigas'];
+let opcoes_16 = ['Park Güell', 'Casa Batlló', 'Sagrada Família',"Antoni Gaudí"];
+let opcoes_17 = ['Pitoresco', "Rococo", "Art-Nouvae", "Barroco"];
+let opcoes_18 = ['Great Sphinx of Giza',"Imhotep", 'Hatshepsut Temple', 'Valley of the Kings'];
+let opcoes_19 = ['Roman Colosseum','Trevi Fountain','Roman Forum','Roma'];
+let opcoes_20 = ['Sede do Partido Comunista Francês','Copan','MAC','Sambodromo'];
+
+//listas
+let listadeperguntas=[pergunta_1,pergunta_2,pergunta_3,pergunta_4,pergunta_5,pergunta_6,pergunta_7,pergunta_8,pergunta_9,pergunta_10,pergunta_11,pergunta_12,pergunta_13,pergunta_14,pergunta_15,pergunta_16,pergunta_17,pergunta_18,pergunta_19,pergunta_20];
+let listadeopcoes=[opcoes_1,opcoes_2,opcoes_3,opcoes_4,opcoes_5,opcoes_6,opcoes_7,opcoes_8,opcoes_9,opcoes_10,opcoes_11,opcoes_12,opcoes_13,opcoes_14,opcoes_15,opcoes_16,opcoes_17,opcoes_18,opcoes_19,opcoes_20];
+let listadegabarito=[gabarito_1,gabarito_2,gabarito_3,gabarito_4,gabarito_5,gabarito_6,gabarito_7,gabarito_8,gabarito_9,gabarito_10,gabarito_11,gabarito_12,gabarito_13,gabarito_14,gabarito_15,gabarito_16,gabarito_17,gabarito_18,gabarito_19,gabarito_20];
+
+//VARIAVEIS
+console.log('recarregou')
+let pontos 
+let indexatual
+
+//Dinamica do Site
+function mudarTexto(id,texto){
+let campo = document.getElementById(id);
+campo.innerHTML = texto;
+}
+
+//Logica para cada turno
+function verificarFim(){
+  console.log('verificando o fim') 
+  let np=listadeperguntas.length 
+  if(indexatual<np-1){
+    indexatual=indexatual+1
+    AtualizarPergunta()}
+  else{
+    mudarTexto('pergunta',`Fim de jogo, obrigado por jogar`)
+    var butoes = document.getElementById("respostas")
+    butoes.style.display = "none";
+    var restart = document.getElementById("start")
+    start.style.display = 'block';
+    
+  }
+}
+
+function AtualizarPergunta(){   
+  console.log(`agora estamos no index ${indexatual}`);
+  let opcaoatual=listadeopcoes[indexatual];
+  let perguntatual=listadeperguntas[indexatual];
+      mudarTexto('pergunta',`${indexatual+1}`+')   '+`${perguntatual}`);
+      mudarTexto('alt_a',opcaoatual[0]);
+      mudarTexto('alt_b',opcaoatual[1]);
+      mudarTexto('alt_c',opcaoatual[2]);
+      mudarTexto('alt_d',opcaoatual[3]);
+      mudarTexto('pontos',`Pontuação:${pontos}`)
+}
+
+function IniciarOJogo(){
+  indexatual=0;
+  pontos=0;
+  AtualizarPergunta();
+  var butoes = document.getElementById("respostas");
+  butoes.style.display = "block"
+  var caixapergunta = document.getElementById("pergunta");
+  caixapergunta.style.display = "block"
+  var comecardesafio = document.getElementById("start");
+  comecardesafio.style.display = "none"
+}
+
+//verificar respostas
+
+function verificarRespostaA(){
+  let respostadada= document.getElementById('alt_a').innerHTML
+  if (respostadada == listadegabarito[indexatual]){
+    console.log('resposta certa')
+    mudarTexto('pergunta',`Resposta Certa!`);
+    pontos= pontos + 10;
+  }
+  else{mudarTexto('pergunta',`Resposta Errada!`)};
+ verificarFim();
+}
+
+function verificarRespostaB(){
+  let respostadada= document.getElementById('alt_b').innerHTML
+  if (respostadada == listadegabarito[indexatual]){
+    mudarTexto('pergunta',`Resposta Certa!`);
+    pontos= pontos + 10;
+    console.log('resposta certa')
+  }
+  else{ mudarTexto('pergunta',`Resposta Errada!`)}
+verificarFim();
+}
+
+function verificarRespostaC(){
+  let respostadada= document.getElementById('alt_c').innerHTML
+  console.log(respostadada)
+  if (respostadada == listadegabarito[indexatual]){
+    mudarTexto('pergunta',`Resposta Certa!`);
+    pontos= pontos + 10;
+    console.log('resposta certa')
+  }
+  else{mudarTexto('pergunta',`Resposta Errada!`)
+      console.log('resposta errada')};
+   verificarFim();
+}
+
+function verificarRespostaD(){
+  let respostadada= document.getElementById('alt_d').innerHTML
+    if (respostadada == listadegabarito[indexatual]){
+    mudarTexto('pergunta',`Resposta Certa!`)
+    pontos= pontos + 10;
+    console.log('resposta certa')
+  } else{
+    mudarTexto('pergunta',`Resposta Errada!`)};
+  verificarFim();
+  }
